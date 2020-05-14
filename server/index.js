70c2e4b1de33e72df658ccffef9f6653a6394d7a
Kevin Geng <kevin.geng@quarksys.ca>
2020-05-14 15:53:50 -0400

    moved receiver code to index

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const https = require('https')
+const http = require('http');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3000
 
 const ACCESS_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAAykEQEAAAAAZTmUIqu2ExG05RwZkPER23J622Y%3DZU4oVKBKY7MYuCb1Og1b71lrEaA7HOFsi3iGhPN5CdyvSEkx1O'
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Database connection
 const mongoose = require('mongoose')
@@ -49,3 +51,45 @@ const cronJob = cron.schedule(`0 * * * * *`, () =>  {
       });
   })
 })
+
+
+
+const getStationInfo = (stationName) => {
+  return https.get('https://myttc.ca/'+stationName+'_station.json', function (result) {
+      var buffer = '';
+      result.setEncoding('utf8');
+      result.on('data', function (data) {
+          buffer += data;
+      });
+      result.on('end', function () {
+          var data = JSON.parse(buffer);
+          console.log(data);
+      });
+  })
+}
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  if (!req.body.Body.indexOf('update')) {
+    twiml.message('This is where we would provide an update on the user\'s settings');
+  } else if (!req.body.Body.indexOf('setup')) {
+    const config = req.body.Body.replace("setup ", "")
+    const pnumber = req.body.From
+
+
+
+    twiml.message('User settings saved.');
+  } else {
+    twiml.message(
+      'User message was not an update or setup request, so no action will be taken'
+    );
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
