19b9da014c4ad563cad14ccae01baa8af04aa9e1
Kevin Geng <kevin.geng@quarksys.ca>
2020-05-14 15:31:54 -0400

    receiver requests from ttc api

--- server/receiver.js
+++ server/receiver.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https')
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
@@ -7,13 +8,34 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', (req, res) => {
+const getStationInfo = (stationName) => {
+  return https.get('https://myttc.ca/'+stationName+'_station.json', function (result) {
+      var buffer = '';
+      result.setEncoding('utf8');
+      result.on('data', function (data) {
+          buffer += data;
+      });
+      result.on('end', function () {
+          var data = JSON.parse(buffer);
+          console.log(data);
+      });
+  })
+}
+
+app.post('/sms', (req, res) => {
+  console.log("gogogo")
   const twiml = new MessagingResponse();
 
-  if (!req.body.Body,indexOf('update')) {
+  if (!req.body.Body.indexOf('update')) {
     twiml.message('This is where we would provide an update on the user\'s settings');
-  } else if (!req.body.Body,indexOf('setup')) {
+  } else if (!req.body.Body.indexOf('setup')) {
     twiml.message('This is where we would provide confirmation that a user\'s settings have been saved');
+  } else if (!req.body.Body.indexOf('station')) {
+    const arr = req.body.Body.split(" ")
+    console.log(arr)
+    info = getStationInfo(arr[1])
+    console.log(info)
+    twiml.message("success");
   } else {
     twiml.message(
       'User message was not an update or setup request, so no action will be taken'
